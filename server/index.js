ba28c2281b93e89f92161a3436351babe1cb28f5
ououou <>
2018-08-07 13:02:11 +0800

    feat: Add Pug and Ejs module learning

--- server/index.js
+++ server/index.js
@@ -1,10 +1,29 @@
 const Koa = require('koa')
 const app = new Koa()
-const { normal } = require('./tpl')
+const { /*htmlTpl, ejsTpl,*/ pugTpl } = require('./tpl')
+// const ejs = require('ejs')
+const pug = require('pug')
+
+// app.use(async (ctx, next) => {
+//     ctx.type = "text/html, charset=utf-8"
+//     ctx.body = htmlTpl
+// })
+
+// app.use(async (ctx, next) => {
+//     ctx.type = "text/html, charset=utf-8"
+//     ctx.body = ejs.render(ejsTpl, {
+//         you: 'Ethan',
+//         me: 'Jinming'
+//     })
+// })
+
 
 app.use(async (ctx, next) => {
-    // ctx.type = "text/html, charset=utf-8"
-    ctx.body = normal
+    ctx.type = "text/html, charset=utf-8"
+    ctx.body = pug.render(pugTpl, {
+        you: 'Ethan',
+        me: 'Jinming'
+    })
 })
 
 app.listen(2333)
